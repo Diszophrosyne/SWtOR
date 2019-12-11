e959352e273b2e2e92b155be9cbd9917bbbfbce3
Diszophrosyne <34870099+Diszophrosyne@users.noreply.github.com>
2019-12-11 17:43:45 -0500

    Add files via upload

--- Team-Parsing-Tracker/bot.js
+++ Team-Parsing-Tracker/bot.js
@@ -1,43 +1,105 @@
 (function () {
 	"use strict";
 	const Discord = require("discord.js"),
-		  auth = require("./auth.json"),
-		  prefix = auth.prefixParse,
-		  client = new Discord.Client({autoReconnect: true});
-	var db = require(./teamDB.json),
+	      auth = require("./auth.json"),
+	      prefix = auth.prefixParse,
+	      client = new Discord.Client({autoReconnect: true});
+	var dbs = require(./teamDBs.json),
 	    nameTeam = function (str) {
-		db.name = str;
+		//db.name = str;
+
+
 	    },
 	    displayTeam = function (name) {
+
+
+
+	    },
+	    listTeams = function () {
+
+
 
 	    },
 	    verifyMember = function (player) {
 
+
+
 	    },
 	    addMember = function (player) {
+		verifyMember(player);
+
 
 	    },
 	    removeMember = function (player) {
+		verifyMember(player);
+
 
 	    },
 	    displayMember = function (player) {
+		verifyMember(player);
+
 
 	    },
 	    updateParse = function (player, spec, value) {
+		verifyMember(player);
+
 
 	    },
 	    addSpec = function (player, spec) {
+		verifyMember(player);
+
 
 	    },
 	    removeSpec = function (player, spec) {
+		verifyMember(player);
+
 
 	    };
 
+
+	client.OWNERID = auth.ownerid;
+
+	// The ready event is vital, it means that your bot will only start reacting to information
+	// from Discord _after_ ready is emitted
+	client.on("ready", () => {
+		console.log("Parse Bot available.");
+	});
+
+	// Create an event listener for messages
+	client.on("message", message => {
+		// It's good practice to ignore other bots. This also makes your bot ignore itself
+		// and not get into a spam loop (we call that "botception").
+
+		if (!(message.author.bot) && (message.content.indexOf(prefix) === 0) ) {
+			var channel = message.channel,
+				input = message.content.substring(4,).toLowerCase();
+
+
+
+
+/*
+			input.length !== 9 ? channel.send("Input has an incorrect number of characters!") : pass++;
+
+			( input.indexOf("/") !== 4 || (input.indexOf("/", 5) !== -1) ) ? channel.send("Delimiter / not found or incorrect format!") : pass++;
+
+			var	[outer, inner] = input.split("/");
+
+			!( /^[bgrpwy]+$/.test(outer) && /^[bgrpwy]+$/.test(inner) ) ? channel.send("Input contains incorrect characters!") : pass++;
+
+			pass === 3 && channel.send(solve(outer, inner));
+*/
+		}
+	});
+
+	// Log our bot in
+	client.login(auth.tokenParse);
+}());
+
+
+
+
+
 ... 
-
-
-
-
 
 
 /*
@@ -75,41 +137,3 @@
 			return resultStr;
 		};
 */
-	client.OWNERID = auth.ownerid;
-
-	// The ready event is vital, it means that your bot will only start reacting to information
-	// from Discord _after_ ready is emitted
-	client.on("ready", () => {
-		console.log("Parse Bot available.");
-	});
-
-	// Create an event listener for messages
-	client.on("message", message => {
-		// It's good practice to ignore other bots. This also makes your bot ignore itself
-		// and not get into a spam loop (we call that "botception").
-
-		if (!(message.author.bot) && (message.content.indexOf(prefix) === 0) ) {
-			var channel = message.channel,
-				input = message.content.substring(4,).toLowerCase(),
-				pass = 0;
-
-
-
-
-/*
-			input.length !== 9 ? channel.send("Input has an incorrect number of characters!") : pass++;
-
-			( input.indexOf("/") !== 4 || (input.indexOf("/", 5) !== -1) ) ? channel.send("Delimiter / not found or incorrect format!") : pass++;
-
-			var	[outer, inner] = input.split("/");
-
-			!( /^[bgrpwy]+$/.test(outer) && /^[bgrpwy]+$/.test(inner) ) ? channel.send("Input contains incorrect characters!") : pass++;
-
-			pass === 3 && channel.send(solve(outer, inner));
-*/
-		}
-	});
-
-	// Log our bot in
-	client.login(auth.tokenParse);
-}());
